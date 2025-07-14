b478971974a644787786abcca5b6e97476d71cba
kakadharma <kakadavidarmawan@gmail.com>
2025-07-14 22:38:10 +0700

    tambah installer

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('qalboost-v1').then(c=>
+      c.addAll([
+        '/', '/index.html', '/style.css', '/script.js',
+        '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'
+      ])
+    )
+  );
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
